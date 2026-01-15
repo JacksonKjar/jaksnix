ccec0b41e44b568bea1ea81abd9fd8c7c861daee
Jackson Kjar <jkjar@amazon.com>
2026-01-15 10:53:06 -0800

    feat: Add default development template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     mac-app-util = {
       url = "github:hraban/mac-app-util";
       inputs.cl-nix-lite.url = "github:r4v3n6101/cl-nix-lite/url-fix";
@@ -15,11 +19,25 @@
   };
 
   outputs = { self, ... }: {
+    # Home Manager modules (cross-platform)
     homeModules = {
       shell = ./modules/shell.nix;
       gui = ./modules/gui.nix;
       nix = ./modules/nix.nix;
       nvf = ./modules/nvf.nix;
+    };
+    
+    # Darwin modules (macOS only)
+    darwinModules = {
+      # Add darwin modules here when ready
+    };
+    
+    # Templates
+    templates = {
+      default = {
+        path = ./templates/default;
+        description = "Basic development environment with direnv";
+      };
     };
   };
 }
---
+++ templates/default/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Add your development tools here
+            ];
+            
+            shellHook = ''
+              echo "Development environment loaded"
+            '';
+          };
+        }
+      );
+    };
+}
